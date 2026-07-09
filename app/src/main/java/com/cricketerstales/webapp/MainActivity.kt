fea6382810711ca673f98390b064363ad81796d3
Nikhil Gowda <nikhilgowda@Nikhils-MacBook-Air.local>
2026-07-09 14:16:49 +0530

    fix: resolve build error caused by missing scale import [release]

--- app/src/main/java/com/cricketerstales/webapp/MainActivity.kt
+++ app/src/main/java/com/cricketerstales/webapp/MainActivity.kt
@@ -75,6 +75,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
